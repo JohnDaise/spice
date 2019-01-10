7009dc9bf03cc6b9b4742ddc2e9e2267184ec22e
JohnDaise <jdaise@gmail.com>
2019-01-10 18:09:37 -0500

    ability to toggle between 'Recipes' and 'Shopping List' pages from nav bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent {
+  @Output() selectedPage = new EventEmitter<string>();
+  
+  onClick(page: string){
+    this.selectedPage.emit(page);
   }
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
 
 }
