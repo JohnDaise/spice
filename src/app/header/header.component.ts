f32cf03b5767c990273cf95fc31cbc44f1baf850
JohnDaise <jdaise@gmail.com>
2019-01-12 12:22:22 -0500

    added styling to header and dropdowns

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @Output() selectedPage = new EventEmitter<string>();
   
-  onClick(page: string){
-    this.selectedPage.emit(page);
-  }
+
   // constructor() { }
 
   // ngOnInit() {
