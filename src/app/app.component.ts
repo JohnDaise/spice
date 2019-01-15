b20ba40d3132d2db8bd7b72d86e8e7d12fd639bd
JohnDaise <jdaise@gmail.com>
2019-01-15 17:21:19 -0500

    setup firebase on init of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   selectedPage = 'recipes';
+  
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyAABDTnRa2us0tIwwXx5xSt60TRIIGy-To",
+      authDomain: "ng-spice.firebaseapp.com"
+    });
+  }
 
   onSelect(page: string){
     this.selectedPage = page;
