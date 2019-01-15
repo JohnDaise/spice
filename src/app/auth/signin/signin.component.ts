41c920fa3d7c1525a490ebf4bde72955dfc1404f
JohnDaise <jdaise@gmail.com>
2019-01-15 18:08:27 -0500

    basic user sign in set up

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -8,7 +9,7 @@ import { NgForm } from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -17,5 +18,6 @@ export class SigninComponent implements OnInit {
   onSignin(form: NgForm){
     const email = form.value.email;
     const password = form.value.password;
+    this.authService.signinUser(email, password);
   }
 }
