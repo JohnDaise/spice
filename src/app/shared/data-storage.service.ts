ddf31bbd546976ceaa355aabe9461e2a799383c7
JohnDaise <jdaise@gmail.com>
2019-01-16 16:04:03 -0500

    updated data-storage service by using 'httpClient'

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -6,25 +6,29 @@ import 'rxjs/RX';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http,
+    constructor(private httpClient: HttpClient,
                 private recipeService: RecipeService,
                 private authService: AuthService){}
 
     storeData(){
         const token = this.authService.getToken();
 
-        return this.http.put('https://ng-spice.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());//use puts bc it will override the database on backend
-    }
+        return this.httpClient.put('https://ng-spice.firebaseio.com/recipes.json?auth=' + token, 
+        this.recipeService.getRecipes());//use puts bc it will override the database on backend
+    } //this section remais the same just use 'httpClient' instead of 'http'
 
     getData(){
         const token = this.authService.getToken();
-         this.http.get('https://ng-spice.firebaseio.com/recipes.json?auth=' + token)
+        //  this.http.get('https://ng-spice.firebaseio.com/recipes.json?auth=' + token)
+         this.httpClient.get<Recipe[]>('https://ng-spice.firebaseio.com/recipes.json?auth=' + token)
             .map(
-                (response: Response) => {
-                  const recipes: Recipe[] = response.json();  
+                // (response: Response) => {
+                    //   const recipes: Recipe[] = response.json();  
+                (recipes) => {
                   for (let recipe of recipes){
                       if (!recipe['ingredients']){
                           console.log(recipe);
