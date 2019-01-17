a5cb0c6f06f56a1e58db665dc2b9cf96fa3a0d11
JohnDaise <jdaise@gmail.com>
2019-01-17 12:27:20 -0500

    added 'StopEdit' action to fix bug where error caused when selecting to update item but navigating away

--- src/app/store/shopping-list.reducers.ts
+++ src/app/store/shopping-list.reducers.ts
@@ -44,21 +44,31 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
             ingredients[state.editedIngredientIndex] = updatedIngredient; //this adds the updated ingredient to the ingredients array
             return {
                 ...state,
-                ingredients: ingredients
+                ingredients: ingredients,
+                editedIngredient: null,
+                editedIngredientIndex: -1
             }; //updates state of ingredients with the new ingredients array
         case ShoppingListActions.DELETE_INGREDIENT:
             const oldIngredients = [...state.ingredients]; 
+            oldIngredients.splice(state.editedIngredientIndex, 1);
             return {
                 ...state,
-                ingredients: state.ingredients.splice(state.editedIngredientIndex, 1)
+                ingredients: oldIngredients,
+                editedIngredient: null,
+                editedIngredientIndex: -1
             };
         case ShoppingListActions.START_EDIT:
             const editedIngredient = {...state.ingredients[action.payload]};
-
         return {
                 ...state,
                 editedIngredient: editedIngredient,
                 editedIngredientIndex: action.payload
+            };
+        case ShoppingListActions.STOP_EDIT:
+        return {
+                ...state,
+                editedIngredient: null,
+                editedIngredientIndex: -1
             };
         default:
             return state;
